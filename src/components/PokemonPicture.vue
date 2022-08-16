e7e7e19736bc14cba75014798ac29630d2ecc808
Antonio Pérez <64748202+AntPerDev@users.noreply.github.com>
2022-08-16 17:12:25 +0200

    corregido css

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="pokemon-container">
 
-        <img class="hidden-pokemon" :src="imgSrc" alt="pokemon">
+        <img v-if="!showPokemon" class="hidden-pokemon" :src="imgSrc" alt="pokemon">
 
-        <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="pokemon">
+        <img v-else class="fade-in" :src="imgSrc" alt="pokemon">
 
     </div>
 
@@ -41,40 +41,16 @@ export default {
 <style scoped>
 .pokemon-container {
     height: 200px;
-    padding: 0;
-    margin: 0;
-    list-style: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 }
-
 img {
-    padding: 5px;
-    width: 200px;
-    /* height: 150px; */
-    margin: 10px;
-    /* line-height: 150px; */
-    color: white;
-    font-weight: bold;
-    font-size: 3em;
-    text-align: center;
-
-
-
-
-    /* height: 200px; */
-    position: absolute;
-    /* right: 36%; */
+    height: 200px;
     user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
     -webkit-user-drag: none;
     -webkit-user-select: none;
 }
-
 .hidden-pokemon {
     filter: brightness(0);
-
 }
 </style>
